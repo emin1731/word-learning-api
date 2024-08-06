515eea1e81cff458371721b81ce62fcba3153a22
emin1731 <emin1731@icloud.com>
2024-08-06 17:38:59 +0400

    Add Module Data Transfer Object

---
+++ src/dto/module.dto.ts
@@ -0,0 +1,15 @@
+import { IsBoolean, IsOptional, IsString, IsUUID, Length } from 'class-validator';
+
+export class ModuleDto {
+	@IsString({ message: 'Not valid module name' })
+	@Length(3, 255, { message: 'Module name must be between 3 and 255 characters' })
+	name: string;
+
+	@IsString({ message: 'Not valid password' })
+	@Length(3, 255, { message: 'Module name must be between 3 and 255 characters' })
+	description: string;
+
+	@IsBoolean({ message: 'Not valid boolean value for isPrivate' })
+	@IsOptional()
+	isPrivate?: boolean;
+}
